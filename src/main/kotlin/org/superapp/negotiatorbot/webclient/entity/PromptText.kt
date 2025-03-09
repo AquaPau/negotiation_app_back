913b440e040021d20f36a6ddb37a29435de42fdc
aquapau <pm.semenova@yandex.ru>
2025-03-09 23:14:26 +0300

    prompts table

---
+++ src/main/kotlin/org/superapp/negotiatorbot/webclient/entity/PromptText.kt
@@ -0,0 +1,23 @@
+package org.superapp.negotiatorbot.webclient.entity
+
+import jakarta.persistence.*
+import org.superapp.negotiatorbot.webclient.enum.LegalType
+
+@Table(name = "prompt_data")
+@Entity
+class PromptText {
+
+    @Id
+    var id: Long? = null
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(nullable = false)
+    var auditory: LegalType? = null
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(nullable = false)
+    var type: LegalType? = null
+
+    @Column
+    var promptText: String? = null
+}
---
+++ src/main/kotlin/org/superapp/negotiatorbot/webclient/enum/LegalType.kt
@@ -0,0 +1,6 @@
+package org.superapp.negotiatorbot.webclient.enum
+
+enum class LegalType {
+
+    INDIVIDUAL, ENTERPRISE
+}
