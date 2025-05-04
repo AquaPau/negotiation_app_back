93d7ea328e65ee310661fba6f762d14c009704fa
polina semenova <pm.semenova@yandex.ru>
2025-05-04 14:47:17 +0200

    document size validation added

--- src/main/kotlin/org/superapp/negotiatorbot/botclient/handler/documentHandler/DocumentHandler.kt
+++ src/main/kotlin/org/superapp/negotiatorbot/botclient/handler/documentHandler/DocumentHandler.kt
@@ -1,12 +1,12 @@
 package org.superapp.negotiatorbot.botclient.handler.documentHandler
 
 import org.springframework.stereotype.Service
-import org.superapp.negotiatorbot.botclient.handler.commandhandler.StartCommand
 import org.superapp.negotiatorbot.botclient.model.TgDocument
 import org.superapp.negotiatorbot.botclient.service.SenderService
 import org.superapp.negotiatorbot.botclient.service.TgDocumentService
 import org.superapp.negotiatorbot.botclient.view.response.CounterpartyTypeQuestion
 import org.superapp.negotiatorbot.webclient.exception.CustomUiException
+import org.superapp.negotiatorbot.webclient.exception.DocumentExceedsSizeException
 import org.superapp.negotiatorbot.webclient.exception.DocumentFormatIsNotAppropriateException
 import org.superapp.negotiatorbot.webclient.exception.DocumentNotReadyForAssistantException
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
@@ -22,12 +22,12 @@ class DocumentHandler(
     private val tgDocumentService: TgDocumentService,
     private val senderService: SenderService,
     private val counterpartyTypeQuestion: CounterpartyTypeQuestion,
-    private val startCommand: StartCommand
 ) {
 
     fun handle(message: Message) {
         try {
             val tgDocument = getTgDocument(message.chat)
+            message.document.validateSize()
             tgDocument.addDocument(message.document)
             tgDocument.addReplyTo(message.messageId)
             tgDocument.validateExtension()
@@ -93,12 +93,18 @@ class DocumentHandler(
         private val APPROPRIATE_EXTENSIONS = listOf("PDF", "DOC", "DOCX", "TXT")
         private const val DEFAULT_EXCEPTION_MESSAGE =
             "Произошла ошибка при выполнении запроса. Пожалуйста, попробуйте снова"
+        private const val MAX_SIZE_IN_BYTES = 10 * 1024 * 1024
         private const val TO_START_MENU_ACTION = "На главную"
         private const val TO_START_MENU = "backToStart"
+
         private fun TgDocument.validateExtension() {
             val extension = this.tgDocumentName?.split(".")?.lastOrNull()?.uppercase()
             if (extension == null || !APPROPRIATE_EXTENSIONS.contains(extension))
                 throw DocumentFormatIsNotAppropriateException()
+        }
+
+        private fun Document.validateSize() {
+            if (this.fileSize > MAX_SIZE_IN_BYTES) throw DocumentExceedsSizeException()
         }
     }
 
